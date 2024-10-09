02214210afba5122bace0b465f3b415fad14e269
onuryigt <onuryigt@gmail.com>
2024-10-09 05:13:01 +0300

    son

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ const db = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: 3306, // Varsayılan MySQL portu
 });
 
 db.connect((err) => {
